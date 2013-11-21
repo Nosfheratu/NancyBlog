5e54299a4e88294b9e1e76468975bf8ecf741721
Fernando Serapio <fhernandosc@gmail.com>
2013-11-21 15:10:31 -0600

    Data layer created. Post Entity & Posts Repository added

---
+++ Nancy.Demo.NancyBlog.Data/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancy.Demo.NancyBlog.Data.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Nancy.Demo.NancyBlog.Data/Repositories/PostsRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nancy.Demo.NancyBlog.Data.Entities;
+
+namespace Nancy.Demo.NancyBlog.Data.Repositories
+{
+    public class PostsRepository
+    {
+        private List<Post> Posts;
+
+        public PostsRepository()
+        {
+            Posts = new List<Post>
+            {
+                new Post(){Id = 1, Title="First Post", Body="This is the first Post", Author="FSC"},
+                new Post(){Id = 2, Title="Second Post", Body="This is the second Post", Author="FSC"},
+                new Post(){Id = 3, Title="Third Post", Body="This is the third Post", Author="FSC"}
+            };
+        }
+
+        public List<Post> GetAll()
+        {
+            return Posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return Posts.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
