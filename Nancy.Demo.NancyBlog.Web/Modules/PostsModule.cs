866744f6b862f112e6b04f996966038cb0f0bbb4
Fernando Serapio <fhernandosc@gmail.com>
2013-11-21 15:12:05 -0600

    Posts Module and Posts views added (index/show)

---
+++ Nancy.Demo.NancyBlog.Web/Modules/PostsModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nancy.Demo.NancyBlog.Data.Entities;
+using Nancy.Demo.NancyBlog.Data.Repositories;
+
+namespace Nancy.Demo.NancyBlog.Web.Modules
+{
+    public class PostsModule : NancyModule
+    {
+        public PostsModule()
+        {
+            PostsRepository repository = new PostsRepository();
+
+            Get["/Posts"] = p =>
+            {
+                return View["Posts/index.cshtml", repository.GetAll()];
+            };
+
+            Get["/Posts/{id}"] = p =>
+            {
+                var post = repository.GetById(p.id);
+
+                return View["Posts/show.cshtml", post];
+            };
+        }
+    }
+}
