6a4f456b6de11565645d97330433d995a7b07d2f
Fernando Serapio <fhernandosc@gmail.com>
2013-11-29 13:10:22 -0600

    added Blogs and Authors repositories

---
+++ Nancy.Demo.NancyBlog.Data/Repositories/AuthorsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nancy.Demo.NancyBlog.Data.Entities;
+using Simple.Data;
+
+namespace Nancy.Demo.NancyBlog.Data.Repositories
+{
+    public class AuthorsRepository
+    {
+        private dynamic db = Database.OpenNamedConnection("NancyBlog");
+        
+        public dynamic Find(int id)
+        {
+            dynamic author = this.db.Authors.FindByAuthorId(id);
+            return author;
+        }
+
+        public List<dynamic> GetAll()
+        {
+            return this.db.Authors.All();
+        }
+
+        public dynamic Add(dynamic author)
+        {
+            var addedAuthor = this.db.Authors.Insert(author);
+            return addedAuthor;
+        }
+
+        public dynamic Update(dynamic author)
+        {
+            this.db.Authors.Update(author);
+            return author;
+        }
+
+        public void Remove(int id)
+        {
+            this.db.Authors.DeleteByAuthorId(id);
+        }
+    }
+}
---
+++ Nancy.Demo.NancyBlog.Data/Repositories/BlogsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nancy.Demo.NancyBlog.Data.Entities;
+using Simple.Data;
+
+namespace Nancy.Demo.NancyBlog.Data.Repositories
+{
+    public class BlogsRepository
+    {
+        private dynamic db = Database.OpenNamedConnection("NancyBlog");
+        
+        public dynamic Find(int id)
+        {
+            dynamic blog = this.db.Blogs.FindByBlogId(id);
+            return blog;
+        }
+
+        public List<dynamic> GetAll()
+        {
+            return this.db.Blogs.All();
+        }
+
+        public dynamic Add(dynamic blog)
+        {
+            var addedBlog = this.db.Blogs.Insert(blog);
+            return addedBlog;
+        }
+
+        public dynamic Update(dynamic blog)
+        {
+            this.db.Blogs.Update(blog);
+            return blog;
+        }
+
+        public void Remove(int id)
+        {
+            this.db.Blogs.DeleteByBlogId(id);
+        }
+    }
+}
