c2fc5bea224b2f7e04d402844a7e0c9753953877
Fernando Serapio <fhernandosc@gmail.com>
2013-11-29 13:20:03 -0600

    Post can be created and updated

--- Nancy.Demo.NancyBlog.Web/Modules/PostsModule.cs
+++ Nancy.Demo.NancyBlog.Web/Modules/PostsModule.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Nancy.ModelBinding;
+using Nancy.Validation;
+
 using Nancy.Demo.NancyBlog.Data.Entities;
 using Nancy.Demo.NancyBlog.Data.Repositories;
 
@@ -12,18 +15,72 @@ namespace Nancy.Demo.NancyBlog.Web.Modules
     {
         public PostsModule()
         {
-            var repository = new PostsRepository();
+            var postsRepository = new PostsRepository();
+            var authorsRepository = new AuthorsRepository();
+            var blogsRepository = new BlogsRepository();
 
             Get["/Posts"] = p =>
             {
-                var posts = repository.GetAll();
+                var posts = postsRepository.GetAll();
+                
                 return View["Posts/index", new { Posts = posts, Title = "Nancy Blog", Description = "A simple blog demo built with Nancy" }];
             };
 
             Get["/Posts/{id}"] = p =>
             {
-                var post = repository.Find(p.id);
+                var post = postsRepository.Find(p.id);
+                
                 return View["Posts/show", new { Post = post, Title = "Nancy Blog", Description = "A simple blog demo built with Nancy" }];
+            };
+
+            Get["/Posts/New"] = p =>
+            {
+                var post = new Post();
+
+                var author = authorsRepository.Find(1);
+
+                var blog = blogsRepository.Find(1);
+
+                return View["Posts/new", new { Post = post, Author = author, Blog = blog, Title = "Nancy Blog", Description = "A simple blog demo built with Nancy", Action = "Create" }];
+            };
+
+            Post["/Posts/Create"] = p =>
+            {
+                Post post = this.Bind();
+
+                var addedPost = postsRepository.Add(post);
+
+                if (post.PostId != addedPost.PostId)
+                {
+                    return Response.AsRedirect("/Posts");
+                }
+
+                return View[""];
+            };
+
+            Get["/Posts/{id}/Edit"] = p =>
+            {
+                var post = postsRepository.Find(p.id);
+
+                var author = authorsRepository.Find(1);
+
+                var blog = blogsRepository.Find(1);
+
+                return View["Posts/edit", new { Post = post, Author = author, Blog = blog, Title = "Nancy Blog", Description = "A simple blog demo built with Nancy", Action = p.id + "/Update" }];
+            };
+
+            Post["/Posts/Update"] = p =>
+            {
+                Post post = this.Bind();
+
+                var modifiedPost = postsRepository.Update(post);
+
+                if (true)
+                {
+                    return Response.AsRedirect("/Posts");
+                }
+
+                return View[""];
             };
         }
     }
